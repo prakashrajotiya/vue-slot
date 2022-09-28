f6dba0b88083890da4a9551ca850ecb8e9d1e8cd
prakashrajotiya <34856687+prakashrajotiya@users.noreply.github.com>
2022-09-28 11:56:25 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <hello
+      >Slot called
+      <template v-slot:myslot>named slot</template>
+    </hello>
+  </div>
+</template>
+
+<script>
+import hello from './components/HelloWorld.vue';
+export default {
+  name: 'App',
+  components: {
+    hello,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
